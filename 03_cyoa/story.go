5c9730ee2359701514250c9b3a3e2d740320c843
LH <lh@lhdv.net>
2018-04-05 16:18:37 -0300

    Second version on json parsing

--- 03_cyoa/story.go
+++ 03_cyoa/story.go
@@ -2,14 +2,12 @@ package cyoa
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"log"
+	"os"
 )
 
-// Adventure main Story structure
-type Adventure struct {
-	content map[string]Content
-}
+// Adventure main adv structure
+type Adventure map[string]Content
 
 // Content groups what will be show on screen
 type Content struct {
@@ -27,72 +25,19 @@ type Option struct {
 // LoadAdventure reads a given json file and create a Arc type with its contents
 func LoadAdventure(file string) Adventure {
 
-	var story interface{}
+	var adv Adventure
 
-	f, err := ioutil.ReadFile(file)
+	f, err := os.Open(file)
 	if err != nil {
 		log.Fatal("Can't load file:", file)
 	}
 
-	if err := json.Unmarshal(f, &story); err != nil {
+	d := json.NewDecoder(f)
+	if err := d.Decode(&adv); err != nil {
 		log.Fatal("Can't parse json file:", file)
 	}
-
-	adv := parseAdventure(story)
 
 	log.Printf("%+v", adv)
 
 	return adv
-}
-
-func parseAdventure(content interface{}) Adventure {
-	var a Adventure
-
-	cc := content.(map[string]interface{})
-
-	a.content = make(map[string]Content, len(cc))
-
-	for k, v := range cc {
-		var cont Content
-		cont = parseContent(v)
-		a.content[k] = cont
-	}
-
-	return a
-}
-
-func parseContent(content interface{}) Content {
-	var c Content
-
-	cc := content.(map[string]interface{})
-
-	c.Title = cc["title"].(string)
-
-	ss := cc["story"].([]interface{})
-	for _, s := range ss {
-		c.Story = append(c.Story, s.(string))
-	}
-
-	oo := cc["options"].([]interface{})
-	for _, o := range oo {
-		c.Options = append(c.Options, parseOption(o))
-	}
-
-	// log.Printf("%s - %q+\n", "[parseContent]", cc)
-	// log.Printf("%s - %q+\n", "[parseContent] type", c)
-
-	return c
-}
-
-func parseOption(content interface{}) Option {
-	var o Option
-
-	c := content.(map[string]interface{})
-	o.Text = c["text"].(string)
-	o.NextArc = c["arc"].(string)
-
-	// log.Printf("%s - %q+\n", "[parseOption]", c)
-	// log.Printf("%s - %q+\n", "[parseOption] type", o)
-
-	return o
 }
