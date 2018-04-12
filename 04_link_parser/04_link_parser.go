4e3da0f63368e7916d4f6cfb3a61af4a932296c6
LH <lh@lhdv.net>
2018-04-12 15:55:34 -0300

    Very rude first version of exercise 04

---
+++ 04_link_parser/04_link_parser.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+	"unicode"
+
+	"golang.org/x/net/html"
+)
+
+// Link group info from a <a> tag
+type Link struct {
+	Href string
+	Text string
+}
+
+func main() {
+
+	var file string
+
+	log.Println("######################")
+	log.Println("[INF] Init application")
+	log.Println("######################")
+
+	file = "ex2.html"
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatal("[ERR] Could not open file " + file)
+	}
+
+	doc, err := html.Parse(f)
+	if err != nil {
+		log.Fatal("[ERR] Could not parse file " + file)
+	}
+
+	l := make([]string, 0)
+	log.Printf("%+q\n", findLinks(doc, &l))
+
+}
+
+func findLinkText(n *html.Node) string {
+	ret := ""
+
+	s := strings.TrimFunc(n.Data, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r) && !unicode.IsPunct(r)
+	})
+	if n.Type == html.TextNode && s != "\n" {
+		return s
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = ret + findLinkText(c)
+	}
+
+	return ret
+}
+
+func findLinks(n *html.Node, links *[]string) []string {
+
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				linkText := findLinkText(n)
+				*links = append(*links, a.Val+";"+linkText)
+				return *links
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		*links = findLinks(c, links)
+	}
+
+	return *links
+}
